1fc6179601e69cadb3a199de802c24de13dde547
VladTZY <tiganilavlad@gmail.com>
2023-07-16 08:37:22 -0700

    GEN-37

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ const {
   UserModel,
   SchoolModel,
   LocationModel,
+  OrganizationModel,
 } = require("../database/sequelize");
 
 const validator = require("validator");
@@ -21,7 +22,14 @@ const createSchool = async (req, res) => {
     phoneNumber,
   } = req.body;
 
-  if (!schoolEmail || !schoolPhoneNumber || !username || !email || !phoneNumber)
+  if (
+    !schoolEmail ||
+    !schoolName ||
+    !schoolPhoneNumber ||
+    !username ||
+    !email ||
+    !phoneNumber
+  )
     throw Error("All fields need to be filled");
 
   if (!validator.isEmail(email)) {
@@ -75,4 +83,80 @@ const createSchool = async (req, res) => {
   }
 };
 
-module.exports = { createSchool };
+const createOrganization = async (req, res) => {
+  const {
+    organizationName,
+    organizationEmail,
+    organizationPhoneNumber,
+    organizationDescription,
+    organizationCountry,
+    organizationCity,
+    organizationLatitude,
+    organizationLongitude,
+    username,
+    email,
+    phoneNumber,
+  } = req.body;
+
+  if (
+    !organizationEmail ||
+    !organizationName ||
+    !organizationPhoneNumber ||
+    !username ||
+    !email ||
+    !phoneNumber
+  )
+    throw Error("All fields need to be filled");
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email invalid");
+  }
+
+  if (!validator.isEmail(organizationEmail)) {
+    throw Error("Organization Email invalid");
+  }
+
+  const exists = await UserModel.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (exists) throw Error("Email already registered");
+
+  const password = "organizationPassword@123";
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await UserModel.create({
+    username: username,
+    email: email,
+    phoneNumber: phoneNumber,
+    password: hash,
+    role: "ORGANIZATION",
+  });
+
+  const location = await LocationModel.create({
+    country: organizationCountry,
+    city: organizationCity,
+    latitude: organizationLatitude,
+    longitude: organizationLongitude,
+  });
+
+  const organization = await OrganizationModel.create({
+    name: organizationName,
+    email: organizationEmail,
+    phoneNumber: organizationPhoneNumber,
+    description: organizationDescription,
+    locationId: location.id,
+    adminId: user.id,
+  });
+
+  res.status(200).json(organization);
+  try {
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+module.exports = { createSchool, createOrganization };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const { requireAuth } = require("../middlewares/requireAuth");
 const { requireAdmin } = require("../middlewares/requireAdmin");
 
-const { createSchool } = require("../controllers/adminController");
+const {
+  createSchool,
+  createOrganization,
+} = require("../controllers/adminController");
 
 router.use(requireAuth);
 router.use(requireAdmin);
 
 router.post("/create_school", createSchool);
+router.post("/create_organization", createOrganization);
 
 module.exports = router;
