05634a6de331be40d01f9bddd2c2bb6570d76357
islav03 <vladconeschi03@gmail.com>
2023-07-30 01:19:39 +0100

    GEN-57

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,18 +4,37 @@ export const Navbar = () => {
   const role = useSelector((state) => state.auth.role);
 
   return (
-    <div>
-      <h1>Bridgy</h1>
+    <div className="flex gap-10 items-center bg-cyan-500">
+      <div>
+        <h1>Bridgy</h1>
+      </div>
 
-      {
+      <div>    
         {
-          STUDENT: <h1>Student ok</h1>,
-          ORGANIZATION: <h1>ORGANIZATION OK</h1>,
-          SCHOOL: <h1>School ok</h1>,
-        }[role]
-      }
+          { 
+            STUDENT:
+               <div className="flex gap-10 items-center bg-cyan-500">
+                <div><h1>Dashboard</h1></div>
+                <div><h1> Find Opportunities</h1></div>
+                <div><h1> Find a Group</h1></div>
+              </div>,
+            ORGANIZATION:
+            <div className="flex gap-10 items-center bg-cyan-500">
+              <div><h1>My Opportunities</h1></div>
+              <div><h1>Post an Opportunity</h1></div>
+            </div>,
+            SCHOOL:
+            <div className="flex gap-10 items-center bg-cyan-500">
+              <div><h1>My Students</h1></div>
+              <div><h1>Notifications</h1></div>
+            </div>,
+          }[role]
+        }
+      </div>
 
-      <h1>profile</h1>
+      <div>
+         <h1>profile</h1>
+      </div>
     </div>
   );
 };
