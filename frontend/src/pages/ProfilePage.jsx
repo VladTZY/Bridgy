547852be12fd435c9298cb932e48e237c7c759ba
VladTZY <tiganilavlad@gmail.com>
2023-08-02 07:41:27 -0700

    GEN-66

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const ProfilePage = () => {
   let { id } = useParams();
-  const name = "Name"
-  const email = "Email"
-  const school = "School"
-  const username = "Username"
-  
-    return (
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://localhost:4004/api/user/profile/${id}`).then((res) => {
+      setUsername(res.data.username);
+      setEmail(res.data.email);
+      setPhoneNumber(res.data.phoneNumber);
+      setRole(res.data.role);
+    });
+  }, [id]);
+
+  return (
     <div className="text-center align-middle">
-      <img className="d-block mx-auto img-fluid w-50" style={{height:150, width:200}} src="https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png"/>
-      <div className="flex-col"> 
-        <h1>{name}</h1>
-        <h1>{email}</h1>
-        <h1>{school}</h1>
+      <img
+        className="d-block mx-auto img-fluid w-50"
+        style={{ height: 150, width: 200 }}
+        src="https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png"
+      />
+      <div className="flex-col">
         <h1>{username}</h1>
+        <h1>{email}</h1>
+        <h1>{phoneNumber}</h1>
+        <h1>{role}</h1>
         <h1>{id}</h1>
       </div>
     </div>
   );
-}
+};
