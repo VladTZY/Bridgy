c97e3d7368dabd75ef02536fc19814d141056fa6
VladTZY <tiganilavlad@gmail.com>
2023-08-02 09:23:14 -0700

    GEN-68

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { NotFoundPage } from "./pages/NotFoundPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import { FindOpportunitiesPage } from "./pages/FindOpportunitiesPage";
 import { ProfilePage } from "./pages/ProfilePage";
+import { OrganizationMyOportunitiesPage } from "./pages/OrganizationMyOportunitiesPage";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
@@ -15,18 +16,89 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={!isLoggedIn ? <Navigate to="/login" /> : {
-          STUDENT: <Navigate to="/student" />,
-          ORGANIZATION: <Navigate to="/organization" />,
-          SCHOOL: <Navigate to="/school" />,
-        }[role] } />
-        <Route path="/login" element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" /> } />
-        <Route path="/student" element={(role == "STUDENT") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/organization" element={(role == "ORGANIZATION") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/school" element={(role == "SCHOOL") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/student/find_opportunities" element={(role == "STUDENT") ? <FindOpportunitiesPage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/profile/:id" element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" /> } />
-        <Route path="*" element={ <NotFoundPage /> } />
+        <Route
+          path="/"
+          element={
+            !isLoggedIn ? (
+              <Navigate to="/login" />
+            ) : (
+              {
+                STUDENT: <Navigate to="/student" />,
+                ORGANIZATION: <Navigate to="/organization" />,
+                SCHOOL: <Navigate to="/school" />,
+              }[role]
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/student"
+          element={
+            role == "STUDENT" ? (
+              <HomePage />
+            ) : isLoggedIn ? (
+              <ErrorPage />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/organization"
+          element={
+            role == "ORGANIZATION" ? (
+              <HomePage />
+            ) : isLoggedIn ? (
+              <ErrorPage />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/school"
+          element={
+            role == "SCHOOL" ? (
+              <HomePage />
+            ) : isLoggedIn ? (
+              <ErrorPage />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/student/find_opportunities"
+          element={
+            role == "STUDENT" ? (
+              <FindOpportunitiesPage />
+            ) : isLoggedIn ? (
+              <ErrorPage />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/organization/my_opportunities"
+          element={
+            role == "ORGANIZATION" ? (
+              <OrganizationMyOportunitiesPage />
+            ) : isLoggedIn ? (
+              <ErrorPage />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/profile/:id"
+          element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
@@ -38,7 +110,7 @@ function WrappedApp() {
       <Navbar />
       <App />
     </BrowserRouter>
-  )
+  );
 }
 
 export default WrappedApp;
---
+++ frontend/src/pages/OrganizationMyOportunitiesPage.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { Card } from "../components/Card";
+
+export const OrganizationMyOportunitiesPage = () => {
+  const jwt = useSelector((state) => state.auth.jwt);
+  const organizationId = useSelector((state) => state.auth.institutionId);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:4004/api/events/by_organization?organizationId=${organizationId}`,
+        {
+          headers: {
+            Authorization: `BEARER ${jwt}`,
+          },
+        }
+      )
+      .then((res) => {
+        setEvents(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [organizationId]);
+  return (
+    <div>
+      {events.map((event) => {
+        return <Card name={event.name} description={event.description} />;
+      })}
+    </div>
+  );
+};
