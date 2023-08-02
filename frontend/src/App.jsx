6a9340b87c7d4e4205a859299180fd05d4c6ff25
islav03 <vladconeschi03@gmail.com>
2023-08-02 05:09:20 +0100

    GEN-65 with id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { LoginPage } from "./pages/LoginPage";
 import { HomePage } from "./pages/HomePage";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate, useParams } from "react-router-dom";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import { FindOpportunitiesPage } from "./pages/FindOpportunitiesPage";
@@ -11,6 +11,7 @@ import { ProfilePage } from "./pages/ProfilePage";
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const role = useSelector((state) => state.auth.role);
+  let { id } = useParams();
 
   return (
     <div>
@@ -21,11 +22,11 @@ function App() {
           SCHOOL: <Navigate to="/school" />,
         }[role] } />
         <Route path="/login" element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" /> } />
-        <Route path="/student" element={(role == "STUDENT") ? <div> <HomePage/></div> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/organization" element={(role == "ORGANIZATION") ? <div>  <HomePage/></div> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/school" element={(role == "SCHOOL") ? <div>  <HomePage/></div> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
+        <Route path="/student" element={(role == "STUDENT") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
+        <Route path="/organization" element={(role == "ORGANIZATION") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
+        <Route path="/school" element={(role == "SCHOOL") ? <HomePage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
         <Route path="/student/find_opportunities" element={(role == "STUDENT") ? <FindOpportunitiesPage/> : (isLoggedIn ? <ErrorPage /> : <Navigate to="/login" />) } />
-        <Route path="/profile/?????" element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" /> } />
+        <Route path="/profile/:id" element={isLoggedIn ? ProfilePage(id) : <Navigate to="/login" /> } />
         <Route path="*" element={ <NotFoundPage /> } />
       </Routes>
     </div>
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const ProfilePage = () => {
+export const ProfilePage = (id) => {
   const name = "Name"
   const email = "Email"
   const school = "School"
