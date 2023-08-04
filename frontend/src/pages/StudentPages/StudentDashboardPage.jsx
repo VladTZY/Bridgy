856e4641252196957b7009cc620ef96c80acdf21
VladTZY <tiganilavlad@gmail.com>
2023-08-04 08:26:31 -0700

    GEN-76

--- frontend/src/pages/StudentPages/StudentDashboardPage.jsx
+++ frontend/src/pages/StudentPages/StudentDashboardPage.jsx
@@ -1,3 +1,43 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import axios from "axios";
+
+import { Card } from "../../components/Card";
+
 export const StudentDashboardPage = () => {
-  return <div>StudentDashboardPage</div>;
+  const jwt = useSelector((state) => state.auth.jwt);
+  const [ongoingEvents, setOngoingEvents] = useState([]);
+  const [pastEvents, setPastEvents] = useState([]);
+  const [requestedEvents, setRequestedEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4004/api/student/ongoing_events", {
+        headers: {
+          Authorization: `BEARER ${jwt}`,
+        },
+      })
+      .then((res) => {
+        setOngoingEvents(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [jwt]);
+
+  return (
+    <div>
+      <div>
+        <h1>Ongoing events</h1>
+        {ongoingEvents.map((data) => {
+          return (
+            <Card
+              key={data.event.id}
+              name={data.event.name}
+              description={data.event.description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
