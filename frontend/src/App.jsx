c4da2f39e353bad1d781ec07863bad99f167adee
VladTZY <tiganilavlad@gmail.com>
2023-08-17 07:35:43 -0700

    GEN-95

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { LoginPage } from "./pages/MiscPages/LoginPage";
 import { HomePage } from "./pages/MiscPages/HomePage";
 import { Navbar } from "./components/Navbar";
@@ -22,159 +23,157 @@ import { LandingPage } from "./pages/MiscPages/LandingPage";
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const role = useSelector((state) => state.auth.role);
+  const location = useLocation();
 
   return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            !isLoggedIn ? (
-              <LandingPage />
-            ) : (
-              {
-                STUDENT: <Navigate to="/student/dashboard" />,
-                ORGANIZATION: <Navigate to="/organization/dashboard" />,
-                SCHOOL: <Navigate to="/school/dashboard" />,
-              }[role]
-            )
-          }
-        />
-        <Route
-          path="/login"
-          element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/student/dashboard"
-          element={
-            role == "STUDENT" ? (
-              <HomePage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/organization/dashboard"
-          element={
-            role == "ORGANIZATION" ? (
-              <HomePage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/school"
-          element={
-            role == "SCHOOL" ? (
-              <HomePage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/student/find_opportunities"
-          element={
-            role == "STUDENT" ? (
-              <FindOpportunitiesPage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/student/dashboard"
-          element={
-            role == "STUDENT" ? (
-              <StudentDashboardPage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/organization/missions"
-          element={
-            role == "ORGANIZATION" ? (
-              <OrganizationMyOportunitiesPage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/organization/post_opportunities"
-          element={
-            role == "ORGANIZATION" ? (
-              <PostOpportunitiesPage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/school/dashboard"
-          element={
-            role == "SCHOOL" ? (
-              <SchoolStudentsMyPage />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/school/add_student"
-          element={
-            role == "SCHOOL" ? (
-              <AddStudent />
-            ) : isLoggedIn ? (
-              <ErrorPage />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/profile/:id"
-          element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/oportunity/:id"
-          element={isLoggedIn ? <OportunityPage /> : <Navigate to="/login" />}
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          !isLoggedIn ? (
+            <LandingPage />
+          ) : (
+            {
+              STUDENT: <Navigate to="/student/dashboard" />,
+              ORGANIZATION: <Navigate to="/organization/dashboard" />,
+              SCHOOL: <Navigate to="/school/dashboard" />,
+            }[role]
+          )
+        }
+      />
+      <Route
+        path="/login"
+        element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/student/dashboard"
+        element={
+          role == "STUDENT" ? (
+            <HomePage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/organization/dashboard"
+        element={
+          role == "ORGANIZATION" ? (
+            <HomePage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/school"
+        element={
+          role == "SCHOOL" ? (
+            <HomePage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/student/find_opportunities"
+        element={
+          role == "STUDENT" ? (
+            <FindOpportunitiesPage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/student/dashboard"
+        element={
+          role == "STUDENT" ? (
+            <StudentDashboardPage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/organization/missions"
+        element={
+          role == "ORGANIZATION" ? (
+            <OrganizationMyOportunitiesPage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/organization/post_opportunities"
+        element={
+          role == "ORGANIZATION" ? (
+            <PostOpportunitiesPage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/school/dashboard"
+        element={
+          role == "SCHOOL" ? (
+            <SchoolStudentsMyPage />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/school/add_student"
+        element={
+          role == "SCHOOL" ? (
+            <AddStudent />
+          ) : isLoggedIn ? (
+            <ErrorPage />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/profile/:id"
+        element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/oportunity/:id"
+        element={isLoggedIn ? <OportunityPage /> : <Navigate to="/login" />}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
 function WrappedApp() {
-  const role = useSelector((state) => state.auth.role);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   return (
     <BrowserRouter>
       {
         {
-          true:
+          true: (
             <div>
               <MiniNavbar />
               <div className="space-x-80">
@@ -188,13 +187,23 @@ function WrappedApp() {
                   <MiniFooter />
                 </div>
               </div>
-            </div>,
-          false:
-            <div>
-              <Navbar />
-              <App />
-              <Footer />
             </div>
+          ),
+          false: {
+            true: (
+              <div>
+                <Navbar />
+                <App />
+              </div>
+            ),
+            false: (
+              <div>
+                <Navbar />
+                <App />
+                <Footer />
+              </div>
+            ),
+          }[location.pathname === "/login"],
         }[isLoggedIn]
       }
     </BrowserRouter>
--- frontend/src/pages/MiscPages/LoginPage.jsx
+++ frontend/src/pages/MiscPages/LoginPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/authSlice";
 
+import LoginImage from "../../../Bridgy_Assets/Images/Webpage/Login.png";
+
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -17,24 +19,61 @@ export const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-cyan-500">
-      <h1>Please Log In</h1>
-      <form onSubmit={submitHandler}>
-        <label>
-          <p>Email</p>
-          <input type="text" onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label>
-          <p>Password</p>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
+    <div className="flex flex-wrap h-full">
+      <div className="w-6/12 flex items-center justify-center">
+        <div className="w-8/12">
+          <h1 className="font-semibold text-6xl">Login</h1>
+          <p className="mt-3">
+            Don`t have an account? Check our account policy
+          </p>
+
+          <form onSubmit={submitHandler} className="flex flex-col mt-12">
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full py-4 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-white focus:border-[#2EA0FB]"
+              />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="mt-3 appearance-none border-2 border-gray-200 rounded-xl w-full py-4 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-white focus:border-[#2EA0FB]"
+              />
+            </div>
+
+            <div className="flex justify-between mt-5">
+              <div className="flex">
+                <input type="checkbox" className="checked:bg-[#2135D9]"></input>
+                <p className="text-gray-500 ml-2">Remember me</p>
+              </div>
+              <p className="text-gray-500">Forgot password?</p>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="mt-10 btn w-full bg-[#2EA0FB] p-5 rounded-[40px] text-white text-3xl hover:bg-[#2e80fb]"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <hr className="w-full h-px my-10 bg-[#2EA0FB] border-0" />
         </div>
-      </form>
+      </div>
+
+      <div className="w-6/12">
+        <img
+          src={LoginImage}
+          className="w-5/12 absolute bottom-0 right-0"
+        ></img>
+      </div>
     </div>
   );
 };
