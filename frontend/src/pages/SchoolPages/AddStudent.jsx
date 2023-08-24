00254ef90d29581e393bc5d4cf7b044ed4d356eb
islav03 <vladconeschi03@gmail.com>
2023-08-24 18:02:09 +0100

    GEN-121

--- frontend/src/pages/SchoolPages/AddStudent.jsx
+++ frontend/src/pages/SchoolPages/AddStudent.jsx
@@ -30,39 +30,46 @@ export const AddStudent = () => {
   };
 
   return (
-    <div className="text-center align-middle bg-cyan-500">
-      <h1>Add one student</h1>
-      <form onSubmit={submitHandler}>
-        <label>
-          <p>Name</p>
-          <input
+    <div className="m-5 p-5 rounded-3xl bg-white">
+      <h1 className="text-3xl font-semibold">Add One Student</h1>
+      <div className="m-5">
+        <form onSubmit={submitHandler}>
+          <label className="my-6">
+            <p className="text-xl">Name</p>
+            <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            className="my-2 rounded-lg w-1/3 p-2 border-2 border-gray-400"
+            placeholder="Name..."
           />
         </label>
-        <label>
-          <p>Email</p>
+        <label className="my-6">
+          <p className="text-xl">Email</p>
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email..."
+            className="my-2 rounded-lg w-1/3 p-2 border-2 border-gray-400"
           />
         </label>
-        <label>
-          <p>Phone Number</p>
+        <label className="my-6">
+          <p className="text-xl">Phone Number</p>
           <input
             type="number"
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
+            className="my-2 rounded-lg w-1/3 p-2 border-2 border-gray-400"
           />
         </label>
-        <div>
-          <button className="bg-white mt-5" type="submit">
+        <div className="self-center">
+          <button className="mt-10 text-white bg-[#2EA0FB] hover:bg-[#2135D9] rounded-full px-20 py-4 text-xl" type="submit">
             Add student
           </button>
         </div>
       </form>
+      </div>
     </div>
   );
 };
