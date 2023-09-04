eb3ef7ce01d37cf7317d9ec42234dfc9700e859b
VladTZY <tiganilavlad@gmail.com>
2023-09-04 09:46:16 -0700

    Sort all student alphabetical

--- frontend/src/pages/SchoolPages/SchoolDashboardPage.jsx
+++ frontend/src/pages/SchoolPages/SchoolDashboardPage.jsx
@@ -7,7 +7,9 @@ import { StudentsTable } from "../../components/StudentsTable";
 export const SchoolStudentsMyPage = () => {
   const jwt = useSelector((state) => state.auth.jwt);
   const [students, setStudents] = useState([]);
+  const [tableData, setTableData] = useState([]);
   const [grade, setGrade] = useState("9");
+  const [sorted, setSorted] = useState("none");
 
   useEffect(() => {
     axios
@@ -16,26 +18,56 @@ export const SchoolStudentsMyPage = () => {
           Authorization: `BEARER ${jwt}`,
         },
       })
-      .then((res) => setStudents(res.data))
+      .then((res) => {
+        setStudents(res.data);
+        setTableData(res.data);
+      })
       .catch((error) => console.log(error));
   }, [jwt, grade]);
+
+  const handleOrderChange = (value) => {
+    if (value == "none") setTableData(students);
+
+    if (value == "alphabetical") {
+      const sortedArray = [...students].sort((a, b) =>
+        a.username.toLowerCase() > b.username.toLowerCase()
+          ? 1
+          : b.username.toLowerCase() > a.username.toLowerCase()
+          ? -1
+          : 0
+      );
+      setTableData(sortedArray);
+    }
+
+    setSorted(value);
+  };
 
   return (
     <div className="h-full bg-gray-100 flex flex-col p-5">
       <div className="flex justify-between">
         <h1 className="text-4xl font-semibold">Students</h1>
-        <select
-          value={grade}
-          onChange={(e) => setGrade(e.target.value)}
-          className="bg-gray-100 border-2 border-gray-300 p-2 rounded-md"
-        >
-          <option value="9">9th Grade</option>
-          <option value="10">10th Grade</option>
-          <option value="11">11th Grade</option>
-          <option value="12">12th Grade</option>
-        </select>
+        <div className="flex">
+          <select
+            value={sorted}
+            onChange={(e) => handleOrderChange(e.target.value)}
+            className="bg-gray-100 border-2 border-gray-300 p-2 rounded-md mr-2"
+          >
+            <option value="none">Order</option>
+            <option value="alphabetical">Alphabetical</option>
+          </select>
+          <select
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+            className="bg-gray-100 border-2 border-gray-300 p-2 rounded-md"
+          >
+            <option value="9">9th Grade</option>
+            <option value="10">10th Grade</option>
+            <option value="11">11th Grade</option>
+            <option value="12">12th Grade</option>
+          </select>
+        </div>
       </div>
-      <StudentsTable students={students} />
+      <StudentsTable students={tableData} />
     </div>
   );
 };
