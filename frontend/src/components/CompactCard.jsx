ba43aaebf772269099448618016839471927cae8
Flavian Calin <flaviancalin@Flavians-MacBook-Pro.local>
2024-01-16 17:13:04 +0200

    branchnouupdate

--- frontend/src/components/CompactCard.jsx
+++ frontend/src/components/CompactCard.jsx
@@ -26,19 +26,19 @@ export const CompactCard = ({
   return (
     <button
       onClick={handleMoreInfo}
-      className="w-[28%] flex flex-col rounded-xl transform transition duration-500  hover:scale-105 hover:drop-shadow-black bg-white m-2"
+      className="w-[28%] h-[40vh] flex flex-col rounded-xl transform transition duration-500  hover:scale-105 hover:drop-shadow-black bg-white m-2 margin-y-auto"
     >
       <img
         src={photoUrl}
-        className="rounded-lg px-2  mt-4 self-center object-cover"
-        style={{ height: "55%", aspectRatio: "16 / 9 , width-[100%], px-6" }}
+        className="rounded-lg px-3  pt-4 self-center object-cover"
+        style={{ height: "55%", aspectRatio: "16 / 9 , width-[100%]," }}
       />
-      <div className="mt-2 ml-6 text-2xl text-bold">{title}</div>
-      <div className="mx-6 mt-2 text-lg text-gray-700">
+      <div className="pt-2 mx-auto px-4 text-xl text-bold w-[100%] "><p className="text-clip overflow-hidden">{title}</p></div> 
+      <div className="mx-auto pt-2 px-2 text-lg text-gray-700">
         {description?.toString().substring(0, 30)}...
       </div>
-      <div className="flex h-[10%] w-full mt-2 justify-center px-3">
-        <div className="flex px-4 border justify-around items-center w-[45%] px-3">
+      <div className="flex h-[10%] w-full mt-2  justify-center px-3">
+        <div className="flex px-3 border justify-around items-center w-[45%]">
           <img src={CalendarIcon} style={{ height: "50%" }} />
           <div className="text-md text-black">{dateToStr(time)}</div>
         </div>
