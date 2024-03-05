45383dbc2e3b829822d703ef836440db8a86508e
Flavian Calin <flaviancalin@Flavians-MacBook-Pro.local>
2024-03-05 19:13:14 +0200

    5mar

--- frontend/src/components/BarOpportunity.jsx
+++ frontend/src/components/BarOpportunity.jsx
@@ -20,21 +20,21 @@ export const BarOpportunity = ({
 
   return (
     <button
-      class="w-[85vw] px-4 transform transition duration-200  hover:scale-[1.01] hover:drop-shadow-black"
+      class="w-[85vw] px-2 transform transition duration-200  hover:scale-[1.01] hover:drop-shadow-black"
       onClick={handleMoreInfo}
     >
-      <div class="flex gap-3 bg-white border-1 shadow rounded-xl overflow-hidden items-center justify-start">
-        <div class="relative w-[15%] lg:w-[3%] pl-4 ">
-          <img className="w-[50%]" loading="lazy" src={circle_src} />
+      <div class="flex bg-white border-1 shadow rounded-xl overflow-hidden items-center justify-start w-[100%] ">
+        <div class=" w-[10%] lg:w-[3%] pl-4 ">
+          <img className="w-[100%] lg:w-[50%]" loading="lazy" src={circle_src} />
         </div>
         <div className="flex justify-between w-[100%] mx-5">
           <div class="flex flex-col  py-2">
-            <p class="text-2xl text-left font-bold">{title}</p>
+            <p class="text-l text-left font-bold">{title}</p>
 
-            <p class="text-gray-500 text-left w-[100%]">{description}</p>
+            <p class="text-gray-500 text-left text-sm w-[100%] truncate">{description}</p>
           </div>
           <div class="flex flex-col  py-2">
-            <p class="text-xl text-right">{location}</p>
+            <p class="text-l text-right">{location}</p>
 
             <p class="text-l text-right">
               <time>{dateToStr(time)}</time>
