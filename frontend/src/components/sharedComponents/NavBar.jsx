1ef676b0181c3276770cb593851663e88c5d54e7
VladTZY <tiganilvlad@gmail.com>
2024-03-31 18:24:27 +0300

    problem fixed

--- frontend/src/components/sharedComponents/NavBar.jsx
+++ frontend/src/components/sharedComponents/NavBar.jsx
@@ -37,7 +37,7 @@ function stringAvatar(name) {
     sx: {
       bgcolor: stringToColor(name),
     },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    children: `${name[0]}`,
   };
 }
 
@@ -84,6 +84,7 @@ function ResponsiveAppBar() {
             >
               <NotificationsIcon />
             </Badge>
+            <Avatar {...stringAvatar(name)} />
           </Toolbar>
         </Toolbar>
       </Container>
