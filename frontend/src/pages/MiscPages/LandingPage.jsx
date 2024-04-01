9f3047e4e8b699f5031c53102048021d315ee0c1
islav03 <vladconeschi03@gmail.com>
2024-04-01 03:05:38 +0100

    Landing Page Text Update

--- frontend/src/pages/MiscPages/LandingPage.jsx
+++ frontend/src/pages/MiscPages/LandingPage.jsx
@@ -27,7 +27,7 @@ export const LandingPage = () => {
               Welcome to Bridgy
               <div className="text-black text-2xl md:text-3xl pt-5 ">
                 <span className="animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent transition duration-100">
-                  Connecting passion with purpose!{" "}
+                  Connecting passion with purpose{" "}
                 </span>
               </div>
               <div className="text-black text-lg md:text-xl pt-5 w-[100%] md:w-[80%] font-light md:font-semibold">
@@ -42,6 +42,30 @@ export const LandingPage = () => {
               >
                 Sign me up
               </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-[100vw] bg-gray-100 justify-center items-center text-center pb-10">
+          <div className="flex flex-col md:flex md:flex-row">
+            <div className="text-4xl md:text-6xl text-black font-bold flex flex-col justify-center items-center text-center md:w-[50%]">
+              <span className="animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent pb-1">
+                Bridgy{" "}
+              </span>
+              <span>is your specialized ecosystem</span>
+              <div className="flex flex-col text-black text-lg md:text-xl pt-10 w-[100%] md:w-[80%] px-6 xs:font-light md:font-semibold pb-5">
+                <span>
+                  Bridgy empowers students, supports schools, and bolsters
+                  charitable efforts, creating a cohesive community dedicated to
+                  service and growth.
+                </span>
+              </div>
+            </div>
+            <div className="relative md:w-[50%] bg-gray-100 ">
+              <img
+                className="  rounded-2xl  bg-gray-100 transform ease-in-out duration-1500"
+                src={pic7}
+                alt="Dashboard"
+              />
             </div>
           </div>
         </div>
@@ -64,15 +88,14 @@ export const LandingPage = () => {
           <div className="flex flex-col md:flex-row w-[100%] bg-gray-100 pt-5 justify-center items-center md:px-6">
             <div className="flex flex-col w-[80%] h-[30vh]  bg-gray-200 rounded-2xl m-5 p-5 items-center justify-center">
               <span className="text-center font-semibold md:text-xl">
-                Finding and obtaining the right community service missions takes
-                an average of 15 hours per opportunity for students.
+                Finding community service missions takes 15 hours per
+                opportunity on average.
               </span>
             </div>
             <div className="flex flex-col w-[80%] h-[30vh] bg-gray-200 rounded-2xl m-5 p-5 items-center justify-center">
               <span className="text-center font-semibold md:text-xl">
-                Charities struggle to find a consistent source of dynamic and
-                motivated volunteers for their missions, hampering their scale
-                and operational capabilities.
+                Charities struggle to find a reliable source of dynamic
+                volunteers, hampering their scale and operational capabilities.
               </span>
             </div>
             <div className="flex flex-col w-[80%]   h-[30vh] bg-gray-200 rounded-2xl m-5 p-5 items-center justify-center">
@@ -83,34 +106,6 @@ export const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-[100vw] bg-gray-100 justify-center items-center text-center pb-10">
-          <div className="flex flex-col md:flex md:flex-row">
-            <div className="text-4xl md:text-6xl text-black font-bold flex flex-col justify-center items-center text-center md:w-[50%]">
-              <span className="animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent pb-1">
-                Bridgy{" "}
-              </span>
-              <span>is your specialized ecosystem</span>
-              <div className="flex flex-col text-black text-lg md:text-xl pt-10 w-[100%] md:w-[80%] px-6 xs:font-light md:font-semibold pb-5">
-                <span>
-                  Welcome to Bridgy, where connecting passion with purpose is
-                  our mission. This platform bridges the gap between high school
-                  students eager to make a difference, charities in need of
-                  enthusiastic volunteers, and educational institutions seeking
-                  streamlined service programs. Our tool is designed to empower
-                  students, support schools, and bolster charitable efforts,
-                  creating a cohesive community dedicated to service and growth.
-                </span>
-              </div>
-            </div>
-            <div className="relative md:w-[50%] bg-gray-100 ">
-              <img
-                className="  rounded-2xl  bg-gray-100 transform ease-in-out duration-1500"
-                src={pic7}
-                alt="Dashboard"
-              />
-            </div>
-          </div>
-        </div>
         <div className="flex flex-col w-[100vw] md:h-[90vh] bg-gray-100 justify-center items-center text-center pb-10">
           <div className="text-4xl md:text-6xl text-black font-bold flex flex-col justify-center items-center text-center">
             <span>What We Offer </span>
@@ -118,50 +113,44 @@ export const LandingPage = () => {
           <div className="flex flex-col md:flex-row w-[100%] bg-gray-100 pt-5 px-4 justify-center items-center md:px-6">
             <div className="flex flex-col w-[100%]  bg-gray-200 rounded-2xl m-5 p-5 items-center justify-evenly h-[50vh] xl:h-[60vh] 2xl:h-[50vh]">
               <div className="text-3xl font-extrabold text-center animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent">
-                {" "}
-                Embrace a new era of educational excellence and community
-                service with Bridgy{" "}
+                Educational and community service excellence
               </div>
               <div className="flex flex-col items-center">
                 <img className="w-[20%] pb-2" src={pic10} alt="schoolicon" />
               </div>
               <span className="text-center font-semibold md:text-l">
-                Bridgy offers schools a seamless way to integrate service
-                learning into their curriculum. Our tools reduce the
-                administrative load dramatically. This enables counselors to
-                spend more time guiding students than on paperwork. With our
-                platform, educators can track student progress, schedule
-                support, and generate insightful reports with ease.
+                Educators can track student progress, schedule support, and
+                generate insightful reports instantly with Bridgy. Our tool
+                reduces administrative load and enables counselors to spend more
+                time guiding students than doing paperwork.
               </span>
             </div>
             <div className="flex flex-col w-[100%] bg-gray-200 rounded-2xl m-5 p-5 items-center justify-evenly h-[50vh] xl:h-[60vh] 2xl:h-[50vh]">
               <div className="text-3xl font-extrabold text-center animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent">
-                Solving your recruitment management
+                Recruitment management for charities
               </div>
               <div className="flex flex-col items-center">
                 <img className="w-[20%] pb-2" src={pic9} alt="charityicon" />
               </div>
               <span className="text-center font-semibold md:text-l">
-                {" "}
-                Your cause deserves the spotlight and dedicated volunteers ready
-                to take action. Bridgy simplifies the volunteer-matching
-                process, connecting your charity with students who share your
-                passion for impact.
+                Charitable causes deserve more spotlight and dedicated
+                volunteers ready to take action. Bridgy simplifies the
+                volunteer-matching process, connecting your charity with
+                students who share your passion for impact.
               </span>
             </div>
             <div className="flex flex-col w-[100%]  bg-gray-200 rounded-2xl m-5 p-5 items-center justify-evenly h-[50vh] xl:h-[60vh] 2xl:h-[50vh]">
               <div className="text-3xl font-extrabold text-center animate-pulse bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 bg-clip-text text-transparent">
-                {" "}
-                Access to opportunity{" "}
+                Student efficiency
               </div>
               <div className="flex flex-col items-center">
                 <img className="w-[20%] pb-2" src={pic8} alt="studenticon" />
               </div>
               <span className="text-center font-semibold md:text-l">
-                As a student Bridgy is your digital compass to finding
+                As a student, Bridgy is your digital compass to finding
                 opportunities that resonate with your interests and aspirations.
-                With our hub, you can connect with meaningful projects in a
-                snap, track your accomplishments, and build real-time
+                Our hub connects you with meaningful projects in a snap, allows
+                you to track your accomplishments, and build real-time
                 assessments of your impact on the community.
               </span>
             </div>
