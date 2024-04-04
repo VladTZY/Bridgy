4630425d6a542c888f6755a85428f48743038200
VladTZY <tiganilvlad@gmail.com>
2024-04-04 10:59:15 +0300

    Notifications fix

--- frontend/src/components/sharedComponents/NotificationCard.jsx
+++ frontend/src/components/sharedComponents/NotificationCard.jsx
@@ -1,8 +1,10 @@
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
+import {
+  ListItemIcon,
+  ListItemText,
+  ListItem,
+  CircularProgress,
+} from "@mui/material";
 import { Link } from "react-router-dom";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
@@ -23,26 +25,42 @@ const notificationsMapping = {
 };
 
 export const NotificationCard = ({ type, message, studentId, eventId }) => {
-  return (
-    <Link to={`/opportunity/${eventId}`}>
-      <ListItem
-        sx={{
-          bgcolor: "white.main",
-          borderRadius: 6,
-          mb: 2,
-          ":hover": {
-            boxShadow: 10,
-          },
-        }}
-      >
-        <ListItemIcon>
-          <CheckCircleOutlineIcon />
-        </ListItemIcon>
-        <ListItemText
-          primary={message}
-          secondary={"Do not forget to show up!"}
-        />
-      </ListItem>
-    </Link>
-  );
+  const value = notificationsMapping[type];
+
+  if (value) {
+    return (
+      <Link to={`/opportunity/${eventId}`}>
+        <ListItem
+          sx={{
+            bgcolor: "white.main",
+            borderRadius: 6,
+            mb: 2,
+            ":hover": {
+              boxShadow: 10,
+            },
+          }}
+        >
+          <ListItemIcon>
+            <value.icon />
+          </ListItemIcon>
+          <ListItemText primary={message} secondary={value.secondary} />
+        </ListItem>
+      </Link>
+    );
+  }
+
+  /*return (
+    <ListItem
+      sx={{
+        bgcolor: "white.main",
+        borderRadius: 6,
+        mb: 2,
+        ":hover": {
+          boxShadow: 10,
+        },
+      }}
+    >
+      <CircularProgress />
+    </ListItem>
+  );*/
 };
